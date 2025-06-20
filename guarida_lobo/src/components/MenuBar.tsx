e655bb6a0df3b5b2dd2202c79bbbbb7dc9f0bc5f
Linx306 <hassam306@gmail.com>
2025-06-19 22:46:28 -0600

    Creacion de la plantilla para la pagina de guarida del lobo

---
+++ guarida_lobo/src/components/MenuBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MenuBar.css';
+
+const MenuBar = () => (
+  <div className="menu-bar">
+    <select><option>MARVEL</option></select>
+    <select><option>DC</option></select>
+    <select><option>[OTRA EDIT]</option></select>
+    <select><option>[OTRA EDIT]</option></select>
+  </div>
+);
+
+export default MenuBar;
