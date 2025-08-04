bf0c2246e60bf97cfe4b9756c010b99920a81f34
Ever789 <evericardo.pd@gmail.com>
2025-08-04 05:18:34 -0600

    Posiblemente funcione

--- guarida_lobo/src/components/Registro.tsx
+++ guarida_lobo/src/components/Registro.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Registro.css';
 
 export default function Registro() {
   const [nombre, setNombre] = useState('');
